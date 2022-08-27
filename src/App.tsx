6c89e73369020848e09dc9058f80619fff828a93
Levi Liester <levi.liester@gmail.com>
2022-08-26 22:01:13 -0500

    Stuff and whatnot

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [engagements, setEngagements] = useState([])
+    useEffect(()=>{
+        axios.get('https://community-engagement.herokuapp.com/api/organizations')
+            .then(({data})=> setEngagements(data.data))
+    },[])
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          Engagements
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div>
+            {engagements.map(engagement => <div>{JSON.stringify(engagement)}</div>)}
+        </div>
+
     </div>
   )
 }
