d2cb84cebfca7fc7b379b720b4a4ddc244c443a4
Levi Liester <levi.liester@gmail.com>
2022-08-31 21:50:19 -0500

    MANTINE

--- src/common/flexcontainer/FlexContainer.tsx
+++ src/common/flexcontainer/FlexContainer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import styles from './FlexContainer.module.css';
+import styles from './FlexContainer.module.scss';
 
 const FlexContainer = (props:any) => {
     const {
--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,15 +1,43 @@
-import './Login.css'
+import styles from "./Login.module.scss";
 import FlexContainer from "../common/flexcontainer/FlexContainer";
 
 function Login() {
   return (
-    <FlexContainer>
-        <div>
-            Left
-        </div>
-        <div>Right</div>
+    <FlexContainer
+      className={styles.loginContainer}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <FlexContainer
+        alignItems={"center"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        flex={1}
+        className={styles.left}
+      >
+        <span className={styles.welcomeText}>
+          Welcome to Community Engagement
+        </span>
+        <FlexContainer
+          alignItems={"center"}
+          justifyContent={"center"}
+          flexDirection={"column"}
+          className={styles.authenticationInputs}
+        >
+          <input placeholder={"john@example.com"} />
+          <input type={"password"} />
+        </FlexContainer>
+      </FlexContainer>
+      <FlexContainer
+        flexDirection={"column"}
+        flex={1}
+        justifyContent={"center"}
+        className={styles.right}
+      >
+        <div>Make a Dream.</div>
+      </FlexContainer>
     </FlexContainer>
-  )
+  );
 }
 
-export default Login
+export default Login;
