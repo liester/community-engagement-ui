d88f87c608dd992532daf48f2affccc3829a3659
Levi Liester <levi.liester@gmail.com>
2022-08-31 22:19:32 -0500

    login form goodness

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,7 +1,45 @@
+import { useForm } from "@mantine/form";
+import { TextInput, Button, Group } from "@mantine/core";
 import styles from "./Login.module.scss";
 import FlexContainer from "../common/flexcontainer/FlexContainer";
+import { IconAt, IconLock } from "@tabler/icons";
 
 function Login() {
+  const form = useForm({
+    initialValues: {
+      password: "",
+      email: "",
+    },
+  });
+
+  const authenticationFields = (
+    <FlexContainer
+      alignItems={"center"}
+      justifyContent={"center"}
+      flexDirection={"column"}
+      className={styles.authenticationInputs}
+    >
+      <TextInput
+        label="Email"
+        placeholder="Email"
+        {...form.getInputProps("email")}
+        icon={<IconAt size={14} />}
+        withAsterisk
+      />
+      <TextInput
+        mt={"md"}
+        label="Password"
+        placeholder="password"
+        {...form.getInputProps("password")}
+        icon={<IconLock size={14} />}
+        withAsterisk
+      />
+      <Group position="center" mt="xl">
+        <Button variant="outline">Log In</Button>
+      </Group>
+    </FlexContainer>
+  );
+  console.log({ form });
   return (
     <FlexContainer
       className={styles.loginContainer}
@@ -15,18 +53,9 @@ function Login() {
         flex={1}
         className={styles.left}
       >
-        <span className={styles.welcomeText}>
-          Welcome to Community Engagement
-        </span>
-        <FlexContainer
-          alignItems={"center"}
-          justifyContent={"center"}
-          flexDirection={"column"}
-          className={styles.authenticationInputs}
-        >
-          <input placeholder={"john@example.com"} />
-          <input type={"password"} />
-        </FlexContainer>
+        <span className={styles.welcomeText}>Welcome To</span>
+        <span className={styles.welcomeText}>Community Engagement</span>
+        {authenticationFields}
       </FlexContainer>
       <FlexContainer
         flexDirection={"column"}
