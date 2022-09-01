36283bef72ff560099b57ae22c588d9c243fc2cd
Levi Liester <levi.liester@gmail.com>
2022-08-31 22:43:08 -0500

    You can auth, check console for results cause ya

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -3,6 +3,7 @@ import { TextInput, Button, Group } from "@mantine/core";
 import styles from "./Login.module.scss";
 import FlexContainer from "../common/flexcontainer/FlexContainer";
 import { IconAt, IconLock } from "@tabler/icons";
+import axios from "../utils/axios";
 
 function Login() {
   const form = useForm({
@@ -30,12 +31,33 @@ function Login() {
         mt={"md"}
         label="Password"
         placeholder="password"
+        type={"password"}
         {...form.getInputProps("password")}
         icon={<IconLock size={14} />}
         withAsterisk
       />
       <Group position="center" mt="xl">
-        <Button variant="outline">Log In</Button>
+        <Button
+          variant="outline"
+          onClick={() => {
+            console.log({ form: form.values });
+            const { email, password } = form.values;
+            axios
+              .post("/api/auth/local", {
+                identifier: email,
+                password: password,
+              })
+              .then((response) => {
+                console.log("User profile", response.data.user);
+                console.log("User token", response.data.jwt);
+              })
+              .catch((error) => {
+                console.log("An error occurred:", error.response);
+              });
+          }}
+        >
+          Log In
+        </Button>
       </Group>
     </FlexContainer>
   );
@@ -47,15 +69,23 @@ function Login() {
       alignItems={"center"}
     >
       <FlexContainer
-        alignItems={"center"}
         flexDirection={"column"}
         justifyContent={"center"}
         flex={1}
         className={styles.left}
       >
-        <span className={styles.welcomeText}>Welcome To</span>
-        <span className={styles.welcomeText}>Community Engagement</span>
-        {authenticationFields}
+        <span className={styles.haveAnAccount}>Have an account? Sign In</span>
+        <FlexContainer
+          alignItems={"center"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          flex={1}
+          className={styles.left}
+        >
+          <span className={styles.welcomeText}>Welcome To</span>
+          <span className={styles.welcomeText}>Community Engagement</span>
+          {authenticationFields}
+        </FlexContainer>
       </FlexContainer>
       <FlexContainer
         flexDirection={"column"}
---
+++ src/utils/axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { BASE_API_URL } from "./constants";
+
+const instance = axios.create({
+  baseURL: BASE_API_URL,
+  timeout: 5000,
+  withCredentials: true,
+});
+
+export default instance;
